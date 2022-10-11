8b9efdab4f8a64139731fdf1aeebb80cb0b50ede
superherono <dmitrii.morozan@gmail.com>
2022-10-11 16:47:11 +0300

    axlxalx

--- js/app.js
+++ js/app.js
@@ -3851,30 +3851,6 @@
                 modules_flsModules.fullpage.switchingSection(blockId);
             }));
         }));
-        const minute = 1e3 * 60, hour = 60 * minute, day = 24 * hour;
-        const dayBlock = document.querySelector(".timer-about__digits--day");
-        const hoursBlock = document.querySelector(".timer-about__digits--hours");
-        const minutesBlock = document.querySelector(".timer-about__digits--minutes");
-        let today = new Date, dd = String(today.getDate()).padStart(2, "0"), mm = String(today.getMonth() + 1).padStart(2, "0"), yyyy = today.getFullYear(), nextYear = yyyy + 1, dayMonth = "10/11/", birthday = dayMonth + yyyy, currentDate = new Date("October 11, 2022 18:30:00");
-        today = mm + "/" + dd + "/" + yyyy;
-        if (today > birthday) birthday = dayMonth + nextYear;
-        const getRemaningTime = function() {
-            const now = (new Date).getTime(), distance = countDown - now;
-            console.log(`now: ${now}`);
-            console.log(`distance: ${distance}`);
-            console.log(`birthday: ${birthday}`);
-            console.log(`countDown: ${countDown}`);
-            dayBlock.innerText = Math.floor(distance / day), hoursBlock.innerText = Math.floor(distance % day / hour), 
-            minutesBlock.innerText = Math.floor(distance % hour / minute);
-            if (distance < 0) {
-                console.log("finished");
-                clearInterval(x);
-            }
-        };
-        const countDown = new Date(currentDate).getTime(), x = setInterval((function() {
-            getRemaningTime();
-        }), 1e3 * 60);
-        getRemaningTime();
         window["FLS"] = false;
         isWebp();
         addTouchClass();
